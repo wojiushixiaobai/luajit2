23a81bfc362638ee7a35d15741d0d14c68f1ebbd
吴小白 <296015668@qq.com>
2024-05-06 13:50:59 +0800

    Add support for LoongArch64

--- src/jit/bcsave.lua
+++ src/jit/bcsave.lua
@@ -99,6 +99,7 @@ local map_arch = {
   mips64r6 =	{ e = "be", b = 64, m = 8, f = 0xa0000407, },
   mips64r6el =	{ e = "le", b = 64, m = 8, f = 0xa0000407, },
   s390x =	{ e = "be", b = 64, m = 22, },
+  loongarch64 =	{ e = "le", b = 64, m = 258, f = 0x3, },
 }
 
 local map_os = {
